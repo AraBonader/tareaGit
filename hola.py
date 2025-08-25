1d2d9ec611c75f51b971db9e566ee077468c5e46
Araceli Bonader <bonaderaraceli@gmail.com>
2025-08-25 16:11:17 -0300

    Agregar codigo a rama-compa2

---
+++ hola.py
@@ -0,0 +1,7 @@
+nombres = ["Ana", "Luis", "Carlos"]
+for nombre in nombres:
+    print(f"Hola {nombre}, bienvenido a la rama-compa2!")
+numeros = [1, 2, 3, 4, 5]
+cuadrados = [n**2 for n in numeros]
+print("Lista de números:", numeros)
+print("Lista de cuadrados:", cuadrados)
